41db87c30ab673ce3a604448796cb82b5893410d
Marcelo Goncalves <marcelocg@gmail.com>
2019-06-04 16:07:18 -0300

    Adopt a more Unix way of scripting by using parameters

--- install.sh
+++ install.sh
@@ -6,48 +6,72 @@ die() {
   exit 1
 }
 
+show_help() {
+  printf '%s\n'   "Installs MCG's most used apps and configs for development purposes."
+  printf '%s\n\n' "Usage: sudo ./install.sh [OPTION]..."
+  printf '%s\n'   "  -p,  --proxy IP     IP address of the proxy server"
+  printf '%s\n\n'   "       --port  PORT   Port number used by the proxy server. Defaults to 3128."
+}
+
 if [ ! "$(id -u)" -eq 0 ] ;then
+  show_help
   die "Please run this script with: \$ sudo $0 $*"
 fi
 
-printf '%s' "Are you behind a proxy (y/n)? "
-old_stty_cfg=$(stty -g)
-stty -icanon -echo
+proxy_ip=
+proxy_port="3128"
 
-# waiting explicitly for y or n:
-behind_proxy=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
+while :; do
+  case $1 in
+    -h|-\?|--help)
+        show_help            # Display usage instructions
+        exit
+        ;;
+    -p|--proxy)              # Ensure option argument has been specified
+        if [ "$2" ]; then
+          proxy_ip=$2
+          shift
+        else
+          die 'ERROR: "-p,  --proxy" requires a non-empty option argument.'
+        fi
+        ;;
+    --proxy=?*)
+        proxy_ip=${1#*=}     # Delete everything up to "=" and assign the remainder.
+        ;;
+    --proxy=)                # Handle the case of an empty --proxy=
+        die 'ERROR: "--proxy" requires a non-empty option argument.'
+        ;;
+    --port)                  # Takes an option argument; ensure it has been specified.
+        if [ "$2" ]; then
+          proxy_port=$2
+          shift
+        else
+          die 'ERROR: "--port" requires a non-empty option argument.'
+        fi
+        ;;
+    --port=?*)
+        proxy_port=${1#*=}   # Delete everything up to "=" and assign the remainder.
+        ;;
+    --port=)                 # Handle the case of an empty --port=
+        die 'ERROR: "--port" requires a non-empty option argument.'
+        ;;
+    --)                      # End of all options.
+        shift
+        break
+        ;;
+    -?*)
+        printf 'WARN: Unknown option (ignored): %s\n' "$1" >&2
+        ;;
+    *)                       # Default case: No more options, so break out of the loop.
+        break
+  esac
 
-if [ "$behind_proxy" != "${behind_proxy#[Yy]}" ] ;then
-  printf '\n\r%s' "Is this a Virtual Machine (y/n)? "
-  vm=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
+  shift
+done
 
-  if [ "$vm" != "${vm#[Yy]}" ] ;then
-    printf '\n\r%s' "VM(W)are or Virtual(B)ox (W/w/B/b)? "
-    hypervisor=$( while ! head -c 1 | grep -i '[bw]' ;do true ;done )
-
-    # I use CNTLM or px proxy at the host machine
-    proxy_port="3128"
-
-    if [ "$hypervisor" != "${hypervisor#[Ww]}" ] ;then
-      proxy_ip="192.168.146.1" #VirtualBox
-    else
-      proxy_ip="192.168.56.1"  #VMWare
-    fi
-    stty $old_stty_cfg
-
-  else # not a VM
-    stty $old_stty_cfg
-    printf '\n\r%s' "Please enter the proxy server IP address: "
-    read -r proxy_ip
-    printf '\n\r%s' "Please enter the proxy server port number: "
-    read -r proxy_port
-  fi
-
-  printf '\n\r%s' "Defining global proxy IP and port as $proxy_ip:$proxy_port"
-  #echo "export http_proxy=http://$proxy_ip:3128" >> /etc/profile
-  #echo "export https_proxy=https://$proxy_ip:3128" >> /etc/profile
-
+if [ "$proxy_ip" ]; then
+  echo "export http_proxy=http://$proxy_ip:$proxy_port" >> /etc/profile
+  echo "export https_proxy=https://$proxy_ip:$proxy_port" >> /etc/profile
 else
-  stty $old_stty_cfg
-  printf '\n\r%s\n' "Ok, no proxy then! Going ahead."
+  echo "No proxy."
 fi
