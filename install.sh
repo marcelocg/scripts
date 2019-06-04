62a6ecf5a0f44b392b42cf04570871668e720ae7
Marcelo <marcelocg@gmail.com>
2019-06-03 22:29:50 -0300

    Ask for proxy IP address when not a VM. More POSIX compliant print.

--- install.sh
+++ install.sh
@@ -14,29 +14,36 @@ stty raw -echo
 behind_proxy=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
 
 if [ "$behind_proxy" != "${behind_proxy#[Yy]}" ] ;then
-  echo -n "\n\rIs this a Virtual Machine (y/n)? "
+  printf \\n\\r%s "Is this a Virtual Machine (y/n)? "
   vm=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
 
   if [ "$vm" != "${vm#[Yy]}" ] ;then
-    echo -n "\n\rVM(W)are or Virtual(B)ox (W/w/B/b)? "
+    printf \\n\\r%s "VM(W)are or Virtual(B)ox (W/w/B/b)? "
     hypervisor=$( while ! head -c 1 | grep -i '[bw]' ;do true ;done )
 
     # I use CNTLM or px proxy at the host machine
+    proxy_port="3128"
+
     if [ "$hypervisor" != "${hypervisor#[Ww]}" ] ;then
-      proxy_ip="192.168.146.1"
+      proxy_ip="192.168.146.1" #VirtualBox
     else
-      proxy_ip="192.168.56.1"
+      proxy_ip="192.168.56.1"  #VMWare
     fi
     stty $old_stty_cfg
-    echo "\n\rDefining global proxy IP as $proxy_ip"
-    echo "export http_proxy=http://$proxy_ip:3128" >> /etc/profile
-    echo "export https_proxy=http://$proxy_ip:3128" >> /etc/profile
+
   else # not a VM
-    # next version will ask for an IP address to set as proxy
     stty $old_stty_cfg
-    echo "\n\rWill ask  for an IP in the near future"
+    printf \\n\\r%s "Please enter the proxy server IP address: "
+    read -r proxy_ip
+    printf \\n\\r%s "Please enter the proxy server port number: "
+    read -r proxy_port
   fi
+
+  printf \\n\\r%s%s%s%s "Defining global proxy IP and port as " $proxy_ip ":" $proxy_port
+  echo "export http_proxy=http://$proxy_ip:3128" >> /etc/profile
+  echo "export https_proxy=https://$proxy_ip:3128" >> /etc/profile
+
 else
   stty $old_stty_cfg
-  echo "\n\rOk, no proxy then! Going ahead."
+  printf \\n\\r%s "Ok, no proxy then! Going ahead."
 fi
